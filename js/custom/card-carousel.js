a456c6025e1c2dac07ab85ce3cf02078902c062a
Draevin Luke <draevinl@gmail.com>
2018-11-29 22:50:31 -0800

    Consolidating carousel and experience fill js

--- js/custom/card-carousel.js
+++ js/custom/card-carousel.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+  var $exp = "https://draevin-new.netlify.com/js/json/experience.json";
+  $.getJSON($exp)
+    .done(function (data) {
+      $.each(data.items, function (i, item) {
+        var $slide = $("<div>", { id: item.name })
+        $slide.appendTo("#experience");
+      });
+    });
+
   $('.card-carousel').slick({
     dots: true,
     arrows: true
