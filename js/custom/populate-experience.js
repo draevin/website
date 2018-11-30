2270771884100f1844828eb9d0c63cc4d7fee180
Draevin Luke <draevinl@gmail.com>
2018-11-29 22:44:22 -0800

    attempting dynamic experience fill test

--- js/custom/populate-experience.js
+++ js/custom/populate-experience.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-
+  var $exp = "https://draevin-new.netlify.com/js/json/experience.json";
+  $.getJSON($exp)
+    .done(function (data) {
+      $.each(data.items, function (i, item) {
+        var $slide = $("<div>", { id: item.name })
+        $slide.appendTo("#experience");
+      });
+    });
 }(jQuery));
