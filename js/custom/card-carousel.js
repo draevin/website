ac0dbf4d33bbdbbb37d3317ce37ca595d449f112
Draevin Luke <draevinl@gmail.com>
2018-12-01 12:59:17 -0800

    Attempting to delay carousel start until after all cards present

--- js/custom/card-carousel.js
+++ js/custom/card-carousel.js
@@ -1,22 +1,32 @@
 $(document).ready(function () {
-  var $exp = "https://draevin-new.netlify.com/js/json/experience.json";
-  $.getJSON($exp)
-    .done(function (data) {
-      $.each(data.exps, function (i, exp) {
-        var $slide = $(document.createElement("div")).attr({
-          id: exp.name,
-          class: "slide purple-dark vert-center-col card-col shadow-def"
-        });
-        var $title = $(document.createElement("h4"));
-        var $titleText = $(document.createTextNode(exp.title));
-        $title.append($titleText);
-        $slide.append($title);
-        $("#experience").append($slide);
-      });
-    });
+  var fillCards = function () {
+    var r = $.Deferred();
 
-  $('.card-carousel').slick({
-    dots: true,
-    arrows: true
-  });
+    var $exp = "https://draevin-new.netlify.com/js/json/experience.json";
+    $.getJSON($exp)
+      .done(function (data) {
+        $.each(data.exps, function (i, exp) {
+          var $slide = $(document.createElement("div")).attr({
+            id: exp.name,
+            class: "slide purple-dark vert-center-col card-col shadow-def"
+          });
+          var $title = $(document.createElement("h4"));
+          var $titleText = $(document.createTextNode(exp.title));
+          $title.append($titleText);
+          $slide.append($title);
+          $("#experience").append($slide);
+        });
+      });
+
+    return r;
+  }
+
+  var startCarousel = function () {
+    $('.card-carousel').slick({
+      dots: true,
+      arrows: true
+    });
+  }
+
+  fillCards().done(startCarousel());
 }(jQuery));
