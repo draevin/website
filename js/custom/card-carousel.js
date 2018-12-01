d97350fe8b0a937da70e468faa11e1f82b05072b
Draevin Luke <draevinl@gmail.com>
2018-12-01 13:35:43 -0800

    More card-carousel fill content

--- js/custom/card-carousel.js
+++ js/custom/card-carousel.js
@@ -24,6 +24,21 @@ $(document).ready(function () {
         var $datesText = $(document.createTextNode(exp.start + " - " + exp.end));
         $slide.append($dates.append($datesText));
 
+        var $desc = $(document.createElement("p"));
+        var $descText = $(document.createTextNode(exp.description));
+        $slide.append($desc.append($descText));
+
+        var $projAccordion = $(document.createElement("div")).attr("class", "accordion");
+        var $projAccordionBtn = $(document.createElement("button")).attr("class", "accordion-btn");
+        var $projPanel = $(document.createElement("div").attr("class", "panel"));
+        var $projList = $(document.createElement("ul"));
+        each(exp.projects, function (i, proj) {
+          var $projItem = $(document.createElement("li"));
+          var $projItemText = $(document.createTextNode(proj));
+          $projList.append($projItem.append($projItemText));
+        });
+        $projAccordion.append($projAccordionBtn.append($projPanel.append($projList)));
+
         $("#experience").append($slide);
       });
       startCarousel();
