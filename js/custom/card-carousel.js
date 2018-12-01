1ff7d87dec0d64ecff6c5fab129273e813feb4a0
Draevin Luke <draevinl@gmail.com>
2018-12-01 13:08:41 -0800

    Actually running the card fill oops

--- js/custom/card-carousel.js
+++ js/custom/card-carousel.js
@@ -1,24 +1,4 @@
 $(document).ready(function () {
-  var fillCards = function () {
-    var r = $.Deferred();
-
-    var $exp = "https://draevin-new.netlify.com/js/json/experience.json";
-    $.getJSON($exp)
-      .done(function (data) {
-        $.each(data.exps, function (i, exp) {
-          var $slide = $(document.createElement("div")).attr({
-            id: exp.name,
-            class: "slide purple-dark vert-center-col card-col shadow-def"
-          });
-          var $title = $(document.createElement("h4"));
-          var $titleText = $(document.createTextNode(exp.title));
-          $title.append($titleText);
-          $slide.append($title);
-          $("#experience").append($slide);
-          startCarousel();
-        });
-      });
-  }
 
   var startCarousel = function () {
     $('.card-carousel').slick({
@@ -27,5 +7,20 @@ $(document).ready(function () {
     });
   }
 
-  // fillCards().done(startCarousel());
+  var $exp = "https://draevin-new.netlify.com/js/json/experience.json";
+  $.getJSON($exp)
+    .done(function (data) {
+      $.each(data.exps, function (i, exp) {
+        var $slide = $(document.createElement("div")).attr({
+          id: exp.name,
+          class: "slide purple-dark vert-center-col card-col shadow-def"
+        });
+        var $title = $(document.createElement("h4"));
+        var $titleText = $(document.createTextNode(exp.title));
+        $title.append($titleText);
+        $slide.append($title);
+        $("#experience").append($slide);
+        startCarousel();
+      });
+    });
 }(jQuery));
